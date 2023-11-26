375f171f54d06f7fd59151eb697e25f267177b36
Tuomas Reunanen <tuomas.reunanen@proton.me>
2023-11-26 11:22:41 +0200

    Implemented periodic backups for tandoor

--- roles/tandoor/templates/backup-tandoor.sh
+++ roles/tandoor/templates/backup-tandoor.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-BACKUPDIR=/var/lib/tandoor-backups
+WORKDIR=/var/lib/tandoor-backups
 
-WORKDIR=$BACKUPDIR/workdir
-DATE=$(date '+%Y-%m-%d_%H:%M:%S')
+DATE=$(date '+%Y-%m-%d_%H-%M-%S')
 
 echo "Ensuring backup & workdirs exist"
 mkdir -p $WORKDIR
@@ -11,17 +10,15 @@ mkdir -p $WORKDIR
 echo "Dumping postgres DB"
 docker exec tandoor-tandoor-db-1 pg_dumpall -U tandoor > $WORKDIR/pgdump.sql
 
-echo "Copying static files"
-cp -r /usr/local/lib/tandoor/staticfiles $WORKDIR/staticfiles
-
 echo "Copying mediafiles"
 cp -r /usr/local/lib/tandoor/mediafiles $WORKDIR/mediafiles
 
 echo "Gzipping all files together"
-gzip $BACKUPDIR/$DATE.gzip $WORKDIR/*
+cd $WORKDIR
+tar czvf $DATE.tar.gz ./*
 
 echo "Uploading backup archive to remote"
-rclone $BACKUPDIR/$DATE.gzip {{ rclone.backup_remote }}:
+rclone copy $WORKDIR/$DATE.tar.gz {{ rclone.backups_remote }}:
 
 echo "Cleaning up"
-rm -rf $BACKUPDIR
+rm -rf $WORKDIR
